de56be218a5376132dc0fd2f6c9aae10e30fa363
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-04-23 11:24:23 +0300

    fix(followers-list): import missing style file

--- src/components/FollowersList/FollowersList.tsx
+++ src/components/FollowersList/FollowersList.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-import 'components/FollowersList/FollowersList';
+import 'components/FollowersList/FollowersList.css';
 
 export interface Follower {
   picture: {
